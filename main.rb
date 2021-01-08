748f753d74ea54ea97057de73678b52fe9745d79
KEI <68884990+kei1992@users.noreply.github.com>
2021-01-08 17:33:44 +0900

    勇者の攻撃設定をリファクタリング

--- main.rb
+++ main.rb
@@ -17,21 +17,41 @@ class Brave
   def attack(monster)
     puts "#{@name}の攻撃"
 
-    # 0~3の間でランダムに数字が変更
-    attack_num = rand(4)
-    if attack_num == 0
-      puts '必殺攻撃'
-      damage = calculate_special_attack - monster.defense
-    else
-      puts '通常攻撃'
-      damage = @offense - monster.defense
-    end
+    attack_type = decision_attack_type
+    damage = calculate_damage(target:monster, attack_type)
 
-    monster.hp -= damage
-
-    puts "#{monster.name}は#{damage}のダメージを受けた"
+    cause_damage(target:monster, damage)
     puts "#{monster.name}の残りHPは#{monster.hp}だ"
   end
+
+
+
+	private
+	def decision_attack_type
+		attack_num = rand(4)
+		if attack_num == 0
+			'special_attack'
+		else
+			'normal_attack'
+		end
+	end
+
+	def calculate_damage(**params)
+		target = params[:target]
+		attack_type = params[:attack_type]
+		if attack_type == 'special_attack'
+			calculate_special_attack - target.damage
+		else
+			damage = @offense - target.defense
+		end
+	end
+
+	def cause_damage(**params)
+		damage = params[:damage]
+		target = params[:target]
+		target.hp -= damage
+		puts "#{monster.name}は#{damage}のダメージを受けた"
+	end
 
   def calculate_special_attack
     @offense * SPECIAL_ATTACK_CONSTANT
