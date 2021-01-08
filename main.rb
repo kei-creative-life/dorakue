b7640e3eba46814ec07041938f12a932c287d8dd
KEI <68884990+kei1992@users.noreply.github.com>
2021-01-08 16:32:49 +0900

    勇者の攻撃を設定

--- main.rb
+++ main.rb
@@ -1,23 +1,37 @@
 class Brave
   # ゲッターを改善
   attr_reader :name, :hp, :offense, :defense
-	attr_accessor :hp
+  attr_accessor :hp
   # initializeを定義
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
-	end
+  end
+
+  def attack(monster)
+    puts "#{@name}の攻撃"
+    damage = @offense - monster.defense
+    monster.hp -= damage
+    puts "#{monster.name}は#{damage}のダメージを受けた"
+    puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
 end
+
+class Monster
+  attr_reader :name, :offense, :defense
+  attr_accessor :hp
+
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+end
+
 brave = Brave.new(name: 'テリー', hp: 500, offense: 150, defense: 100)
+monster = Monster.new(name: 'スライム', hp: 250, offense: 200, defense: 100)
 
-puts <<~TEXT
-  NAME：#{brave.name}
-  HP：#{brave.hp}
-  OFFENSE：#{brave.offense}
-  DEFENSE：#{brave.defense}
-TEXT
-
-brave.hp -= 30
-puts "#{brave.name}はダメージを受けた!残りHPは#{brave.hp}だ"
+brave.attack(monster)
