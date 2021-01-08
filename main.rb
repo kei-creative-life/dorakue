191b8f670c2bd45bec8d4c2814c18900c58d2af6
KEI <68884990+kei1992@users.noreply.github.com>
2021-01-08 16:09:05 +0900

    initializeの改善

--- main.rb
+++ main.rb
@@ -1,38 +1,29 @@
 class Brave
-  # nameセッターの定義
-  attr_writer :name
-
+  # initializeを定義
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
   # nameゲッターの定義
   attr_reader :name
-
-  # hpセッターの定義
-  attr_writer :hp
 
   # hpゲッターの定義
   attr_reader :hp
 
-  # offenseセッターの定義
-  attr_writer :offense
-
   # offenseゲッターの定義
   attr_reader :offense
-
-  # defenseセッターの定義
-  attr_writer :defense
 
   # defenseゲッターの定義
   attr_reader :defense
 end
 
-brave = Brave.new
-brave.name = 'テリー'
-brave.hp = 500
-brave.offense = 150
-brave.defense = 100
+brave = Brave.new(name: 'テリー', hp: 500, offense: 150, defense: 100)
 
 puts <<~TEXT
-NAME：#{brave.name}
-HP：#{brave.hp}
-OFFENSE：#{brave.offense}
-DEFENSE：#{brave.defense}
+  NAME：#{brave.name}
+  HP：#{brave.hp}
+  OFFENSE：#{brave.offense}
+  DEFENSE：#{brave.defense}
 TEXT
