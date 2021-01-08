ddb753aad3214de50ec01c96a9f83928a8ea03b4
KEI <68884990+kei1992@users.noreply.github.com>
2021-01-08 13:43:22 +0900

    セッターの定義

--- main.rb
+++ main.rb
@@ -1,2 +1,24 @@
 class Brave
+	# セッターの定義
+	def name=(name)
+		@name = name
+	end
+
+	def hp=(hp)
+		@hp = hp
+	end
+
+	def offence=(offence)
+		@offence = offence
+	end
+
+	def defence=(defence)
+		@defence = defence
+	end
+
+	brave = Brave.new
+	brave.name = "テリー"
+	brave.hp = 500
+	brave.offence = 150
+	brave.defence = 100
 end
